2afbc46dc3785947aa720e4b77fdee53c6dd45d0
TStepi <tomaz.stepisnik@ijs.si>
2018-03-01 19:48:09 +0100

    Dodal Scheduling nalogo.

--- src/main/SchedulingTask.java
+++ src/main/SchedulingTask.java
@@ -1,4 +1,27 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SchedulingTask implements Solution{
+
+    Vehicle[] vozila;
+    List<Ride> izbrana, neizbrana;
+    int B, T;
+
+    public Solution Mutate(double rate) {
+        return null;
+    }
+
+    public ArrayList<Solution> MateWith(Solution partner) {
+        return null;
+    }
+
+    public double Fitness() {
+        long score = 0;
+        for (int i=0; i<vozila.length; i++){
+            score += vozila[i].Evaluate(T, B);
+        }
+        return score;
+    }
 }
