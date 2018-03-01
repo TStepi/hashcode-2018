09691528b40495f8ccda416106f850ea146e371b
andrej <andrej.borstnik@matheo.si>
2018-03-01 20:09:19 +0100

    ..

--- src/main/SchedulingTask.java
+++ src/main/SchedulingTask.java
@@ -2,14 +2,29 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Random;
 
-public class SchedulingTask implements Solution{
+public class SchedulingTask {
 
     Vehicle[] vozila;
     List<Ride> izbrana, neizbrana;
     int B, T;
+    Random rng;
+
+    public SchedulingTask(Vehicle[] vozila, List<Ride> neizbrana, int B, int T) {
+        rng = new Random();
+        this.B = B;
+        this.T = T;
+        this.neizbrana = neizbrana;
+        this.vozila = vozila;
+    }
 
     public Solution Mutate(double rate) {
+        int n = vozila.length;
+
+        
+
         return null;
     }
 
