ca6f44d70fc5673a5605cf835e1d64b15e239397
TStepi <tomaz.stepisnik@ijs.si>
2018-03-01 19:17:08 +0100

    Vozila in Rajdi.

---
+++ src/main/Vehicle.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vehicle {
+
+    public List<Ride> urnik;
+    int step;
+    int[] location;
+    int score;
+
+    public Vehicle(){
+        urnik = new ArrayList<Ride>();
+    }
+
+    public int Evaluate(int T, int B){
+        step = 0;
+        location = new int[] {0, 0};
+        score = 0;
+
+        for (int i = 0; i<urnik.size(); i++){
+            DoRide(B, urnik.get(i));
+
+            if (step >= T) break;
+        }
+
+        return score;
+    }
+
+    public void DoRide(int B, Ride r){
+        location = r.finishIntersection;
+        score += r.Score(B, step, step + r.length);
+        step += r.length;
+    }
+}
